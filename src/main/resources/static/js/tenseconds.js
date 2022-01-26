fb0067febd7be3f14e2d524bc3572f575d97eef3
Yu_Jehyeop <fbwpguq@gmail.com>
2022-01-26 22:54:46 +0900

    [update] 10s 코드 수정[#4]

--- src/main/resources/static/js/tenseconds.js
+++ src/main/resources/static/js/tenseconds.js
@@ -3,3 +3,35 @@ $(document).ready(function(){
     $(".menu").fadeIn(1000);
     $(".paging_button").fadeIn(1000);
 });
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementsByClassName("item");
+
+function item_click(idx){
+    btn[idx].onclick = function (){
+        modal.style.display = "block";
+    }
+}
+for(var i=0; i<btn.length; i++){
+    item_click(i);
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
