58d63af000d7c27db3c3a89c9dd26f334c39d49e
Yu_Jehyeop <fbwpguq@gmail.com>
2022-01-31 04:05:05 +0900

    [update]10s페이지 modal 구현 및 javascript 작성[#4]

--- src/main/resources/static/js/tenseconds.js
+++ src/main/resources/static/js/tenseconds.js
@@ -2,36 +2,68 @@ $(document).ready(function(){
     $(".container").fadeIn(1000);
     $(".menu").fadeIn(1000);
     $(".paging_button").fadeIn(1000);
+    item_click();
+    getVideos();
 });
+
+function item_click(){
+    $(".item").click(function (){
+        $("#myModal").show();
+        // alert("클릭 확인");
+        close_click();
+        // background_click();
+    });
+}
+
+function close_click(){
+    $(".close").click(function (){
+       $("#myModal").hide();
+    });
+}
+
+// function background_click(){
+//     $(window).click(function (event) {
+//         if (event.target == $("#myModal")){
+//             $("#myModal").hide();
+//         };
+//     });
+// }
 
 var modal = document.getElementById("myModal");
 
-var btn = document.getElementsByClassName("item");
-
-function item_click(idx){
-    btn[idx].onclick = function (){
-        modal.style.display = "block";
-    }
-}
-for(var i=0; i<btn.length; i++){
-    item_click(i);
-}
-
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+function getVideos() {
+    $.ajax({
+        type: "GET",
+        url: "/videos",
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            for (let i = 0; i < response.length; i++) {
+                num = response.length - i;
+                videoListPost(response[i], num);
+                videoInfoPost(response[i], num);
+            }
+        }
+    })
+}
+
+function videoListPost(article, index) {
+    let tempHtml = `<div class="item" id="${index}">${article["video"]}
+                    <a href="#" class="item-title">${article['title']}</a>
+                    </div>
+                    `;
+    $(".item").append(tempHtml);
+}
+
+function videoInfoPost(article, index) {
+    let tempHtml = `<div>
+
+                    </div>
+                    `;
+    $(".modal-content").append(tempHtml);
 }
