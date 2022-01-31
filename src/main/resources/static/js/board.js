f61a18a1d30ec056de54ee280996647ab7f5f843
Lluroa <luora@naver.com>
2022-02-01 00:23:56 +0900

    [add] 글 작성하기 백엔드 연결과정 일부 수정 [#13]

--- src/main/resources/static/js/board.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $(".container").fadeIn(1000);
-    $(".menu").fadeIn(1000);
-    $(".paging_button").fadeIn(1000);
-});
--- src/main/resources/static/js/registry.js
+++ src/main/resources/static/js/registry.js
@@ -4,7 +4,24 @@ $(document).ready(function(){
     $(".paging_button").fadeIn(1000);
 });
 
+function saveArticle() {
+    let form_data = new FormData()
+    form_data.append("title", $("#h1").val())
+    form_data.append("main", $("#h2").val())
 
+    $.ajax({
+        type: "POST",
+        url: `/registry`,
+        data: form_data,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert("성공적으로 업로드 되었습니다.");
+            // sessionStorage.setItem("image_idx", response['idx']);
+            location.href = "/space"; // 페이지 변환
+        }
+    });
+}
 
 
 
