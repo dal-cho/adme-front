f38df4bbe839564afa0f94bb9df5d2aa62e606d1
Lluroa <luora@naver.com>
2022-05-11 20:07:55 +0900

    [add] 메세지 보낼 때마다 사용자 수 새로고침 [#43]

---
+++ src/main/resources/static/js/chat.js
@@ -0,0 +1,50 @@
+let username = sessionStorage.getItem("nickname");
+let socket = new WebSocket("ws://localhost:8080/websocket");
+
+socket.onopen = function (e) {
+    console.log('open server!')
+    let str = username + " 님이 입장하셨습니다.";
+    socket.send(str);
+};
+
+socket.onclose = function (e) { // 채팅방에서 나갔을 때
+    let str = username + " 님이 방을 나가셨습니다.";
+    socket.send(str);
+    $("#join-count").load(window.location.href + " #join-span")
+}
+
+// 접속자 수 띄우기
+$(document).ready(count_user)
+
+function count_user() {
+    $.ajax({
+        type: "GET",
+        url: `/websocket/count`,
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            if (response == 0) {
+                response = 1
+            }
+            $("#num").text(response)
+        }
+    })
+}
+
+
+socket.onerror = function (e) {
+    console.log(e);
+}
+
+socket.onmessage = function (e) {
+    console.log(e.data);
+    let msgArea = document.querySelector('.msgArea');
+    let newMsg = document.createElement('div');
+    newMsg.innerText = e.data;
+    msgArea.append(newMsg);
+}
+
+function sendMsg() {
+    let content = document.querySelector('.content').value;
+    socket.send(username + " : " + content); // 닉네임 : message 형태
+    count_user();
+}
