99bba1bb412bcabb8d6d7660b3cd39441ef1206d
soyeon <luora@naver.com>
2023-01-02 20:24:05 +0900

    [update] back, front [#117]

--- src/main/java/com/dalcho/adme/AdmeFrontApplication.java
+++ src/main/java/com/dalcho/adme/AdmeFrontApplication.java
@@ -1,7 +1,11 @@
 package com.dalcho.adme;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AdmeFrontApplication {
     public static void main(String[] args) {
-        System.out.println();
+        SpringApplication.run(AdmeFrontApplication.class, args);
     }
 }
