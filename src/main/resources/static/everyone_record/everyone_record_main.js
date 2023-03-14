5c8b9f3ba02bfdcd48874a6f7e296b61aac52cb3
Yu_Jehyeop <fbwpguq@gmail.com>
2023-03-14 12:25:47 +0900

    [add] Everyone_Record_Modal [#129]

---
+++ src/main/resources/static/everyone_record/everyone_record_main.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+});
+// 모달 열기
+function videoModal() {
+    $(".modal-video").get(0).play();
+    $(".modal-content").fadeIn(100);
+    $(".modal-container").fadeIn(100);
+}
+
+// 모달 영역 밖 클릭시 닫기
+let modal = document.getElementById("modal");
+window.onclick = function(event) {
+    if (event.target == modal) {
+        $(".modal-video").get(0).pause();
+        $(".modal-content").fadeOut(300);
+        $(".modal-container").fadeOut(300);
+
+    }
+}
+
+// 재생
+function playVideo() {
+    $(".modal-video").get(0).play();
+}
+// 재생 중지
+function pauseVideo() {
+    $(".modal-video").get(0).pause();
+}
+// 음소거
+function mutedVideo() {
+    let muteButton = $(".video-mute-button");
+
+    $(".modal-video").get(0).muted = true;
+    muteButton.css("border", "1px solid #3b3b3b");
+    muteButton.css("background-color", "#858585");
+    muteButton.attr("onclick", "soundVideo()");
+
+}
+// 소리 재생
+function soundVideo() {
+    let muteButton = $(".video-mute-button");
+
+    $(".modal-video").get(0).muted = false;
+    muteButton.css("background", "");
+    muteButton.attr("onclick", "mutedVideo()");
+}
