9a9d32d6db4145338e62685d0d16fb58e1d88fa2
Yu_Jehyeop <fbwpguq@gmail.com>
2023-06-11 03:24:26 +0900

    [add] video 게시글 페이징 설정

--- src/main/resources/static/js/video/video-connection.js
+++ src/main/resources/static/js/video/video-connection.js
@@ -1,33 +1,28 @@
 $(document).ready(function(){
-    getList();
+    getList(1);
 });
 
 // video 전체 조회
-function getList() {
+function getList(currentNumber) {
     console.log("getList");
     $.ajax({
         type: "GET",
-        url: host + '/tenSeconds/list',
+        url: host + '/tenSeconds/list'+currentNumber,
         success: function (response) {
             // alert("getList 동작시작");
-            console.log(response);
-            for (let i=0; i<response.length; i++) {
-                videoListPost(response[i], i);
-            }
-        }
-    })
-}
+            let list = `${response["videoList"]}`;
 
-// video 단건조회
-function videoModal(id) {
-    showModal();
-    $.ajax({
-        type: "GET",
-        url: host + '/tenSeconds/video/'+id,
-        headers: {"X-AUTH-TOKEN": cookie},
-        success: function (response) {
-            console.log(response);
-            // showModal(response);
+            let startPage = `${response["startPage"]}`;
+            let endPage = `${response["endPage"]}`; // 마지막 페이지 번호
+            let prev = `${response["prev"]}`; // 이전 버튼
+            let next = `${response["next"]}`; // 다음 버튼
+
+            $(".video-container").empty();
+
+            for (let i=0; i<list.length; i++) {
+                videoListPost(list[i], i);
+            }
+            makePagination(currentNumber, startPage, endPage, prev, next);
         }
     })
 }
@@ -45,16 +40,63 @@ function videoListPost(article, index) {
     $(".video-container").append(tempHtml);
 }
 
+// 아래 하단 페이징
+function makePagination(currentNumber, startPage, endPage, prev, next) {
+    let tempHtml = ``;
+
+    // 이전 페이지가 있다면 < 표시
+    if (prev) {
+        tempHtml += `<div><a href="#" onclick="beforeClick(${currentNumber})">&lt;</a></div>`
+    }
+
+    // 페이징 번호 표시
+    for (let i = startPage; i <= endPage; i++) {
+        if (currentNumber === i) {
+            tempHtml += `<div><a href="#">${i}</a></div>`;
+        } else {
+            tempHtml += `<div><a href="#" onclick="getArticle(${i})">${i}</a></div>`;
+        }
+    }
+
+    // 다음 페이지가 있다면 < 표시
+    if (next) {
+        tempHtml += `<div><a href="#" onclick="nextClick(${currentNumber})">&gt;</a></div>`
+    }
+
+    $('.paging').html(tempHtml);
+}
+
+function beforeClick(currentNumber) {
+    getList(currentNumber - 1);
+}
+
+function nextClick(currentNumber) {
+    getList(currentNumber + 1);
+}
+
+// video 단건조회
+function videoModal(id) {
+    $.ajax({
+        type: "GET",
+        url: host + '/tenSeconds/video/'+id,
+        headers: {"X-AUTH-TOKEN": cookie},
+        success: function (response) {
+            console.log(response);
+            showModal(response);
+        }
+    })
+}
+
 // 모달에 들어갈 아이템 설정
-function showModal() {
-    // let date = `${response["videoDate"]}`
-    //
-    // $(".modal-title").text(`${response["title"]}`); // 제목
-    // $(".modal-video").attr("id", `${response["id"]}`); // video id 설정
-    // $(".modal-video>source").attr("src", `${response["uploadPath"]}`+ `${response["uuid"]}` + ".mp4"); // video 경로
-    // $(".content-nickname").text(`${response["nickname"]}`); // 게시물 주인 닉네임
-    // $(".content-date").text(date.split("T")[0].replaceAll("-",".")); // 업로드 날짜
-    // $(".video-content>textarea").text(`${response["content"]}`); // 게시물 설명
+function showModal(response) {
+    let date = `${response["videoDate"]}`
+
+    $(".modal-title").text(`${response["title"]}`); // 제목
+    $(".modal-video").attr("id", `${response["id"]}`); // video id 설정
+    $(".modal-video>source").attr("src", `${response["uploadPath"]}`+ `${response["uuid"]}` + ".mp4"); // video 경로
+    $(".content-nickname").text(`${response["nickname"]}`); // 게시물 주인 닉네임
+    $(".content-date").text(date.split("T")[0].replaceAll("-",".")); // 업로드 날짜
+    $(".video-content>textarea").text(`${response["content"]}`); // 게시물 설명
 
     $(".modal-video").get(0).play();
     $(".modal-content").fadeIn(100);
