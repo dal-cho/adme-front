2fed9e730f90255f931768883b56c7c567f71511
soyeon <luora@naver.com>
2024-02-14 00:29:28 +0900

    [update] html [#185]

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -93,7 +93,7 @@ function openChatList() {
                 let days = today.getDate();
                 let now = month + "/" + days
                 let dayTime
-                if(message==null || message.equals("")){
+                if(message==null || message===""){
                     message = " 고객센터 입장하기"
                 }
                 if (now !== day && month!=null) {
